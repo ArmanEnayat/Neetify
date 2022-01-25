82c293f07a2dad1d4a51dd69391a26ff2e6da4ba
Arman Enayat Barbhuiya <79511509+ArmanEnayat@users.noreply.github.com>
2022-01-25 18:09:00 +0530

    Add files via upload

---
+++ partials/footer.php
@@ -0,0 +1,115 @@
+
+    
+  <!-- Footer Section -->
+  
+  <footer class="footer">
+      
+      <!--
+      <div class="footerCol">
+          <h3>Unique Features</h3>
+          <li>Mock Tests</li>
+          <li>Analysis</li>
+          <li>Detailed Report</li>
+          <li>Test Preps</li>
+          <li>Guide</li>
+          <li>Subscriptions</li>
+          <li>Newsletter</li>
+          <li>Contact Us</li>
+      </div>-->
+      
+      <div class="footerCol">
+          <h3>Quick Links</h3>
+          <ul>
+              <li>Home</li>
+              <li>Blogs</li>
+              <li>Terms Of Service</li>
+              <li>Privacy Policy</li>
+              <li>FAQ</li>
+              <li>Help Center</li>
+              <li>Services</li>
+              <li>About Us</li>
+          </ul>
+      </div>
+      
+      <div class="footerCol">
+          <h3>Social Media</h3>
+          <i class="fab fa-facebook sIcon"></i><a href="#" class="fbIcon">Facebook</a>
+          <br>
+          <i class="fab fa-instagram sIcon"></i><a href="#" class="instaIcon">Instagram</a>
+          <br>
+          <i class="fab fa-twitter sIcon"></i><a href="#" class="twitterIcon">Twitter</a>
+          <br>
+          <i class="fab fa-youtube sIcon"></i><a href="https://youtube.com/c/AuWorldgamer" class="ytIcon">Youtube</a>
+      </div>
+      
+      <div class="footerCol addressCont">
+          <h3>Address</h3>
+          <div class="address">
+              <div class="place">
+                  <span class="fas fa-map-marker-alt"></span>
+                  <span class="addressText">Silchar, Assam</span>
+              </div>
+          </div>
+          
+          <div class="address">
+              <div class="phone">
+                  <span class="fas fa-phone-alt"></span>
+                  <span class="phoneText">+91 6000266103</span>
+              </div>
+          </div>
+          
+          <div class="address">
+              <div class="emailAddress">
+                  <span class="fas fa-envelope"></span>
+                  <span class="emailText">superarmanenayat@gmail.com</span>
+              </div>
+          </div>
+      </div>
+      
+      <div class="footerCol">
+          <h3>Newsletter</h3>
+          <p>We'll Only Send Promo, Offers.</p>
+          <div class="subscribeNews">
+              <input type="email" placeholder="Your Email Address" class="inputSubscribe">
+              <a href="#"><input type="submit" name="subscribe" value="SUBSCRIBE"></a>
+          </div>
+      </div>
+      
+      <div class="border"></div>
+      
+      <div class="foot">
+          <div class="socialIcons">
+              <a href="#" class="fbIconS"><i class="fab fa-facebook"></i></a>
+              <a href="#" class="instaIconS"><i class="fab fa-instagram"></i></a>
+              <a href="#" class="twitterIconS"><i class="fab fa-twitter"></i></a>
+              <a href="https://youtube.com/c/AuWorldgamer" class="ytIconS"><i class="fab fa-youtube"></i></a>
+          </div>
+          
+          <br>
+          
+          <div class="creatorText">
+              <p>Copyright © 2022, All Rights Reserved.</p>
+              <p>Created and Developed By Arman Enayat & ARTechStudios. This Template Is Subject To Copyright.</p>
+          </div>
+      </div>
+      
+  </footer>
+  
+  
+</body>
+
+<script>
+      $('#ham').click(function(){
+          $('nav .nav ul').addClass('active');
+      });
+
+      $('#hamClose').click(function() {
+          $('nav .nav ul').removeClass('active');
+      });
+  </script>
+
+
+<!-- Project -->
+<script src="js/main.js"></script>
+
+</html>
---
+++ partials/footerL.php
@@ -0,0 +1,41 @@
+
+    
+  <!-- Footer Section -->
+  
+  <footer class="footerL">
+      
+      <div class="footL">
+          <div class="socialIcons">
+              <a href="#" class="fbIconS"><i class="fab fa-facebook"></i></a>
+              <a href="#" class="instaIconS"><i class="fab fa-instagram"></i></a>
+              <a href="#" class="twitterIconS"><i class="fab fa-twitter"></i></a>
+              <a href="https://youtube.com/c/AuWorldgamer" class="ytIconS"><i class="fab fa-youtube"></i></a>
+          </div>
+          
+          <br>
+          
+          <div class="creatorText">
+              <p>Copyright © 2022, All Rights Reserved.</p>
+              <p>Created and Developed By Arman Enayat & ARTechStudios. This Template Is Subject To Copyright.</p>
+          </div>
+      </div>
+      
+  </footer>
+  
+  <script>
+      $('#ham').click(function(){
+          $('nav .nav ul').addClass('active');
+      });
+
+      $('#hamClose').click(function() {
+          $('nav .nav ul').removeClass('active');
+      });
+  </script>
+  
+</body>
+
+
+<!-- Project -->
+<script src="js/main.js"></script>
+
+</html>
